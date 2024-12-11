b3f4d83a2d2a81f7cb659a3855a5b2f0f242e0bf
YoonMooYeol <agecross@naver.com>
2024-12-11 19:01:48 +0900

    2024/12/11/19:01

---
+++ .p10k.zsh
@@ -0,0 +1,11 @@
+# ...existing code...
+
+# Enable the anaconda segment
+typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(anaconda ...existing elements...)
+
+# Customize the anaconda segment
+typeset -g POWERLEVEL9K_ANACONDA_FOREGROUND=70
+typeset -g POWERLEVEL9K_ANACONDA_BACKGROUND=237
+typeset -g POWERLEVEL9K_ANACONDA_VISUAL_IDENTIFIER_EXPANSION='🐍'
+
+# ...existing code...
